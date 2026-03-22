81a1e15cb956e2a1143bbd6d5987cca9d4be9fed
Shreyanshu M <93574203+shreyanshumis@users.noreply.github.com>
2026-03-22 06:22:22 +0000

    A practice code for type conversion in js.

---
+++ basics/typeconversion.js
@@ -0,0 +1,21 @@
+let score = "33" //String
+// let score = 33 //Integer or number
+
+
+//to see which datatype they belong to
+console.log(typeof score);
+// console.log(typeof (score)); //as a method
+
+//ALL SCENARIOS WITH THIS IN MIND-> type convert from String to Numbers - if you are sure that you are going to do operations on numbers only but get input as string
+
+let valueInNumber  = Number(score)
+console.log(typeof valueInNumber);
+
+// if we have value as "33abcxyz" instead of 33
+// console.log(valueInNumber); -> we get NaN(not a number) but it's type is number
+
+//if we put null then it gives 0
+
+//if we put undefined then it gives NaN again
+
+//if we give boolean value then it gives 0(false) and 1(true)
